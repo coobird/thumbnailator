7e14b83e463b0f8aea45497a4907bed22c66f98a
Chris Kroells <coobird@coobird.net>
2010-09-22 16:29:07 +0900

    Added general contract of ImageFilters to the class javadoc.

--- src/net/coobird/thumbnailator/filters/ImageFilter.java
+++ src/net/coobird/thumbnailator/filters/ImageFilter.java
@@ -5,6 +5,13 @@ import java.awt.image.BufferedImage;
 /**
  * This interface is to be implemented by classes which performs an image
  * filtering operation on a {@link BufferedImage}.
+ * <p>
+ * The general contract for classes implementing {@link ImageFilter} is that
+ * they should not change the contents of the {@link BufferedImage} which is
+ * given as the argument for the {@link #apply(BufferedImage)} method.
+ * <p>
+ * The filter should make a copy of the given {@link BufferedImage}, and
+ * perform the filtering operations on the copy, then return the copy.
  * 
  * @author coobird
  *
