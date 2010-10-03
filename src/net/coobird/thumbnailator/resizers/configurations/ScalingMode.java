3e62724d2bd65b7e919b6f5854df6b37aa26d00f
coobird <coobird@localhost>
2010-10-03 16:46:49 +0900

    * Added javadocs. * Added a package javadoc.

--- src/net/coobird/thumbnailator/resizers/configurations/ScalingMode.java
+++ src/net/coobird/thumbnailator/resizers/configurations/ScalingMode.java
@@ -3,10 +3,33 @@
  */
 package net.coobird.thumbnailator.resizers.configurations;
 
+import net.coobird.thumbnailator.resizers.ProgressiveBilinearResizer;
+
+/**
+ * An enum which is used to specify how to scale images when creating
+ * thumbnails.
+ * 
+ * @author coobird
+ *
+ */
 public enum ScalingMode 
 {
+	/**
+	 * A hint to use bilinear interpolation when resizing images.
+	 */
 	BILINEAR,
+	
+	/**
+	 * A hint to use bicubic interpolation when resizing images.
+	 */
 	BICUBIC,
+	
+	/**
+	 * A hint to use progressing bilinear interpolation when resizing images.
+	 * <p>
+	 * For details on this technique, refer to the documentation of the
+	 * {@link ProgressiveBilinearResizer} class.
+	 */
 	PROGRESSIVE_BILINEAR,
 	;
 }
---
+++ src/net/coobird/thumbnailator/resizers/configurations/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * This package provides enums which are used to set rendering hints used when
+ * using {@link net.coobird.thumbnailator.resizers.Resizers} to create
+ * thumbnails.
+ */
+package net.coobird.thumbnailator.resizers.configurations;
