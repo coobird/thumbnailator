a9c0bba0289eef7cb73d37675c76ba2af440dc05
coobird <coobird@localhost>
2010-10-03 18:39:50 +0900

    * Added javadocs.

--- src/net/coobird/thumbnailator/resizers/configurations/ResizerConfiguration.java
+++ src/net/coobird/thumbnailator/resizers/configurations/ResizerConfiguration.java
@@ -2,12 +2,28 @@ package net.coobird.thumbnailator.resizers.configurations;
 
 import java.awt.RenderingHints;
 
+import net.coobird.thumbnailator.resizers.Resizer;
+
 /**
+ * An interface which are implemented by classes and enums which provide 
+ * configuration information for {@link Resizer}s.
+ * 
  * @author coobird
  *
  */
 public interface ResizerConfiguration
 {
+	/**
+	 * Returns a rendering hint key.
+	 * 
+	 * @return		Rendering hint key.
+	 */
 	public RenderingHints.Key getKey();
+	
+	/**
+	 * Returns a rendering hint value.
+	 * 
+	 * @return		Rendering hint value.
+	 */
 	public Object getValue();
 }
