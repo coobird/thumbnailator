75867e8793c2b1ed4382800442308b21ac0db7e3
Chris Kroells <coobird@coobird.net>
2010-12-19 13:19:07 +0900

    Added source/sink for files and URLs.

---
+++ src/net/coobird/thumbnailator/tasks/io/FileImageSource.java
@@ -0,0 +1,79 @@
+package net.coobird.thumbnailator.tasks.io;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Iterator;
+
+import javax.imageio.ImageIO;
+import javax.imageio.ImageReader;
+import javax.imageio.stream.ImageInputStream;
+
+import net.coobird.thumbnailator.tasks.UnsupportedFormatException;
+
+public class FileImageSource extends AbstractImageSource
+{
+	private static final int FIRST_IMAGE_INDEX = 0;
+	private final File sourceFile;
+	
+	/**
+	 * @param sourceFile
+	 */
+	public FileImageSource(File sourceFile)
+	{
+		super();
+		this.sourceFile = sourceFile;
+	}
+	
+	/**
+	 * @param sourceFile
+	 */
+	public FileImageSource(String sourceFilePath)
+	{
+		super();
+		this.sourceFile = new File(sourceFilePath);
+	}
+
+	public BufferedImage read() throws IOException
+	{
+		if (!sourceFile.exists())
+		{
+			throw new FileNotFoundException(
+					"Could not find file: " + sourceFile.getAbsolutePath()
+			);
+		}
+		
+		/* TODO refactor.
+		 * The following code has been adapted from the 
+		 * StreamThumbnailTask.read method.
+		 */
+		ImageInputStream iis = ImageIO.createImageInputStream(sourceFile);
+		
+		if (iis == null)
+		{
+			throw new IOException(
+					"Could not open file: " + sourceFile.getAbsolutePath());
+		}
+		
+		Iterator<ImageReader> readers = ImageIO.getImageReaders(iis);
+		if (!readers.hasNext())
+		{
+			String sourcePath = sourceFile.getPath();
+			throw new UnsupportedFormatException(
+					UnsupportedFormatException.UNKNOWN,
+					"No suitable ImageReader found for " + sourcePath + "."
+			);
+		}
+		
+		ImageReader reader = readers.next();
+		reader.setInput(iis);
+		inputFormatName = reader.getFormatName();
+
+		BufferedImage img = reader.read(FIRST_IMAGE_INDEX);
+		
+		iis.close();
+		
+		return img;
+	}
+}
---
+++ src/net/coobird/thumbnailator/tasks/io/URLImageSource.java
@@ -0,0 +1,49 @@
+package net.coobird.thumbnailator.tasks.io;
+
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.net.Proxy;
+import java.net.URL;
+
+public class URLImageSource extends AbstractImageSource
+{
+	private final URL url;
+	private final Proxy proxy;
+
+	/**
+	 * @param is
+	 */
+	public URLImageSource(URL url)
+	{
+		super();
+		this.url = url;
+		this.proxy = null;
+	}
+	
+	/**
+	 * @param url
+	 * @param proxy
+	 */
+	public URLImageSource(URL url, Proxy proxy)
+	{
+		super();
+		this.url = url;
+		this.proxy = proxy;
+	}
+
+	public BufferedImage read() throws IOException
+	{
+		InputStreamImageSource source;
+		
+		if (proxy != null)
+		{
+			source = new InputStreamImageSource(url.openConnection(proxy).getInputStream());
+		}
+		else
+		{
+			source = new InputStreamImageSource(url.openStream());
+		}
+		
+		return source.read();
+	}
+}
