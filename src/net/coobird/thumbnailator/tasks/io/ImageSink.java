0983bb2c50a5cb96d4beebf092e04d145c6b6e0c
coobird <coobird@localhost>
2010-12-26 02:30:09 +0900

    Added javadocs.

--- src/net/coobird/thumbnailator/tasks/io/ImageSink.java
+++ src/net/coobird/thumbnailator/tasks/io/ImageSink.java
@@ -5,9 +5,40 @@ import java.io.IOException;
 
 import net.coobird.thumbnailator.ThumbnailParameter;
 
+/**
+ * An interface to be implemented by classes which stores the image resulting
+ * from a thumbnail generation task.
+ * 
+ * @author coobird
+ *
+ */
 public interface ImageSink
 {
+	/**
+	 * Writes the resulting image to a destination.
+	 * 
+	 * @param img				The image to write or store.
+	 * @throws IOException		When a problem occurs while writing or storing
+	 * 							the image.
+	 */
 	public void write(BufferedImage img) throws IOException;
+	
+	/**
+	 * Sets the output format of the resulting image.
+	 * <p>
+	 * For {@link ImageSink}s which stores raw images, the format name specified
+	 * by this method may be ignored.
+	 * 
+	 * @param format			File format with which to store the image.
+	 */
 	public void setOutputFormatName(String format);
+	
+	/**
+	 * Sets the {@link ThumbnailParameter} from which to retrieve parameters
+	 * to use when storing the image.
+	 * 
+	 * @param param				The {@link ThumbnailParameter} with image
+	 * 							writing parameters.
+	 */
 	public void setThumbnailParameter(ThumbnailParameter param);
 }
--- src/net/coobird/thumbnailator/tasks/io/ImageSource.java
+++ src/net/coobird/thumbnailator/tasks/io/ImageSource.java
@@ -3,8 +3,28 @@ package net.coobird.thumbnailator.tasks.io;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 
+/**
+ * An interface to be implemented by classes which read or retrieve images
+ * from which a thumbnail should be produced.
+ * 
+ * @author coobird
+ *
+ */
 public interface ImageSource
 {
+	/**
+	 * Retrieves the image from the source.
+	 * 
+	 * @return					The image.
+	 * @throws IOException		When a problem occurs while reading or obtaining
+	 * 							the image.
+	 */
 	public BufferedImage read() throws IOException;
+	
+	/**
+	 * Returns the name of the image format.
+	 * 
+	 * @return					The image format name.
+	 */
 	public String getInputFormatName();
 }
