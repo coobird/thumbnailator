179dee8efe5ff972e7f92f38605a80cfb4c79029
Chris Kroells <coobird@coobird.net>
2011-02-06 16:19:52 +0900

    Added unit tests for `ImageFilter`s.

---
+++ test/net/coobird/thumbnailator/filter/WatermarkTest.java
@@ -0,0 +1,43 @@
+package net.coobird.thumbnailator.filter;
+
+import static org.junit.Assert.*;
+
+import java.awt.image.BufferedImage;
+
+import net.coobird.thumbnailator.filters.ImageFilter;
+import net.coobird.thumbnailator.filters.Watermark;
+import net.coobird.thumbnailator.geometry.Positions;
+import net.coobird.thumbnailator.test.BufferedImageComparer;
+import net.coobird.thumbnailator.util.BufferedImages;
+
+import org.junit.Test;
+
+/**
+ * Tests for the {@link Watermark} filter.
+ * 
+ * @author coobird
+ *
+ */
+public class WatermarkTest
+{
+	/**
+	 * Checks that the input image contents are not altered.
+	 */
+	@Test
+	public void inputContentsAreNotAltered() 
+	{
+		// given
+		BufferedImage originalImage = new BufferedImage(200, 200, BufferedImage.TYPE_INT_ARGB);
+		BufferedImage copyImage = BufferedImages.copy(originalImage);
+		
+		BufferedImage watermarkImg = new BufferedImage(50, 50, BufferedImage.TYPE_INT_ARGB);
+		
+		ImageFilter filter = new Watermark(Positions.BOTTOM_CENTER, watermarkImg, 0.5f);
+		
+		// when
+		filter.apply(originalImage);
+		
+		// then
+		assertTrue(BufferedImageComparer.isSame(originalImage, copyImage));
+	}
+}
