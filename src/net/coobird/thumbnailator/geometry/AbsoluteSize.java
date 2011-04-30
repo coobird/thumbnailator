7ffa38bff0f2e62b975df253ff5a86fd2d4969fe
coobird <coobird@localhost>
2011-04-30 17:41:34 +0900

    Added interface and implementation classes to determine the size depending on the size of the enclosing object.

---
+++ src/net/coobird/thumbnailator/geometry/AbsoluteSize.java
@@ -0,0 +1,23 @@
+package net.coobird.thumbnailator.geometry;
+
+import java.awt.Dimension;
+
+public class AbsoluteSize implements Size
+{
+	private final Dimension size;
+	
+	public AbsoluteSize(Dimension size)
+	{
+		this.size = new Dimension(size);
+	}
+	
+	public AbsoluteSize(int width, int height)
+	{
+		this.size = new Dimension(width, height);
+	}
+
+	public Dimension calculate(int width, int height)
+	{
+		return new Dimension(size);
+	}
+}
---
+++ src/net/coobird/thumbnailator/geometry/RelativeSize.java
@@ -0,0 +1,21 @@
+package net.coobird.thumbnailator.geometry;
+
+import java.awt.Dimension;
+
+public class RelativeSize implements Size
+{
+	private final double scalingFactor;
+	
+	public RelativeSize(double scalingFactor)
+	{
+		super();
+		this.scalingFactor = scalingFactor;
+	}
+
+	public Dimension calculate(int width, int height)
+	{
+		int newWidth = (int)Math.round(width * scalingFactor);
+		int newHeight = (int)Math.round(height * scalingFactor);
+		return new Dimension(newWidth, newHeight);
+	}
+}
