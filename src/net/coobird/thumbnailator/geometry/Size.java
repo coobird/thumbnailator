51082f295f1a650823b74873efa9846f5c506a69
coobird <coobird@localhost>
2011-05-01 14:10:07 +0900

    Added checks to the `calculate` method to reject non-positive widths and heights.

--- src/net/coobird/thumbnailator/geometry/Size.java
+++ src/net/coobird/thumbnailator/geometry/Size.java
@@ -14,11 +14,13 @@ public interface Size
 	/**
 	 * Calculates the size of the object. 
 	 * 
-	 * @param width			The width of the object which encloses the object 
+	 * @param width			Width of the object which encloses the object 
 	 * 						for which the size should be determined.
-	 * @param height		The height of the object which encloses the object 
+	 * @param height		Height of the object which encloses the object 
 	 * 						for which the size should be determined.
-	 * @return				The calculated size of the object.
+	 * @return				Calculated size of the object.
+	 * @throws IllegalArgumentException	If the width and/or height is less than
+	 * 									or equal to {@code 0}.	
 	 */
 	public Dimension calculate(int width, int height);
 }
