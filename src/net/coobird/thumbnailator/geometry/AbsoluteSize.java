6d498254c64b7d274c4711f80ee2c1bc371069ca
Chris Kroells <coobird@coobird.net>
2011-05-01 14:07:12 +0900

    Added parameter checks.

--- src/net/coobird/thumbnailator/geometry/AbsoluteSize.java
+++ src/net/coobird/thumbnailator/geometry/AbsoluteSize.java
@@ -20,9 +20,14 @@ public class AbsoluteSize implements Size
 	 * Instantiates an object which indicates size of an object.
 	 * 
 	 * @param size		Size of the enclosed object.
+	 * @throws NullPointerException		If the size is {@code null}.
 	 */
 	public AbsoluteSize(Dimension size)
 	{
+		if (size == null)
+		{
+			throw new NullPointerException("Size cannot be null.");
+		}
 		this.size = new Dimension(size);
 	}
 	
@@ -31,9 +36,17 @@ public class AbsoluteSize implements Size
 	 * 
 	 * @param width		Width of the enclosed object.
 	 * @param height	Height of the enclosed object.
+	 * @throws IllegalArgumentException		If the width and/or height is less
+	 * 										than or equal to {@code 0}.
 	 */
 	public AbsoluteSize(int width, int height)
 	{
+		if (width <= 0 || height <= 0)
+		{
+			throw new IllegalArgumentException(
+					"Width and height must be greater than 0."
+			);
+		}
 		this.size = new Dimension(width, height);
 	}
 
