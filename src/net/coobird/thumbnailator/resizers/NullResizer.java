7d3aed8e6bb33683f7e3ad91aa943a45380a8703
Chris Kroells <coobird@coobird.net>
2011-05-21 19:56:35 +0900

    Added null argument check test cases to unit tests. Chnaged the `NullResizer` class to be a subclass of `AbstractResizer` to perform the needed checks, and the above unit tests now pass.

--- src/net/coobird/thumbnailator/resizers/NullResizer.java
+++ src/net/coobird/thumbnailator/resizers/NullResizer.java
@@ -1,7 +1,11 @@
 package net.coobird.thumbnailator.resizers;
 
 import java.awt.Graphics;
+import java.awt.RenderingHints;
+import java.awt.RenderingHints.Key;
 import java.awt.image.BufferedImage;
+import java.util.Collections;
+import java.util.Map;
 
 /**
  * A {@link Resizer} which does not actually resize the image.
@@ -11,10 +15,25 @@ import java.awt.image.BufferedImage;
  * @author coobird
  *
  */
-public class NullResizer implements Resizer
+public class NullResizer extends AbstractResizer
 {
+	public NullResizer()
+	{
+		this(
+				RenderingHints.VALUE_INTERPOLATION_BILINEAR,
+				Collections.<Key, Object>emptyMap()
+		);
+	}
+	
+	private NullResizer(Object interpolationValue, Map<Key, Object> hints)
+	{
+		super(interpolationValue, hints);
+	}
+
 	public void resize(BufferedImage srcImage, BufferedImage destImage)
 	{
+		super.performChecks(srcImage, destImage);
+		
 		Graphics g = destImage.getGraphics();
 		g.drawImage(srcImage, 0, 0, null);
 		g.dispose();
