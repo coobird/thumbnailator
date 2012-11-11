6991f68beb6db214e7ab60baef2fe26f1669c671
coobird <coobird@localhost>
2012-11-11 19:37:01 +0900

    Added a utility class to create `ImageFilter`s required to rotate/flip according to the EXIF orientation value.

---
+++ src/net/coobird/thumbnailator/util/exif/ExifFilterUtils.java
@@ -0,0 +1,49 @@
+package net.coobird.thumbnailator.util.exif;
+
+import net.coobird.thumbnailator.filters.Flip;
+import net.coobird.thumbnailator.filters.ImageFilter;
+import net.coobird.thumbnailator.filters.Pipeline;
+import net.coobird.thumbnailator.filters.Rotation;
+
+/**
+ * @author coobird
+ *
+ */
+public final class ExifFilterUtils {
+	
+	/**
+	 * This class should not be instantiated. 
+	 */
+	private ExifFilterUtils() {};
+	
+	public static ImageFilter getFilterForOrientation(Orientation orientation) {
+		Pipeline filters = new Pipeline();
+		
+		if (orientation == Orientation.TOP_RIGHT) {
+			filters.add(Flip.HORIZONTAL);
+			
+		} else if (orientation == Orientation.BOTTOM_RIGHT) {
+			filters.add(Rotation.ROTATE_180_DEGREES);
+			
+		} else if (orientation == Orientation.BOTTOM_LEFT) {
+			filters.add(Rotation.ROTATE_180_DEGREES);
+			filters.add(Flip.HORIZONTAL);
+			
+		} else if (orientation == Orientation.LEFT_TOP) {
+			filters.add(Rotation.RIGHT_90_DEGREES);
+			filters.add(Flip.HORIZONTAL);
+			
+		} else if (orientation == Orientation.RIGHT_TOP) {
+			filters.add(Rotation.RIGHT_90_DEGREES);
+			
+		} else if (orientation == Orientation.RIGHT_BOTTOM) {
+			filters.add(Rotation.LEFT_90_DEGREES);
+			filters.add(Flip.HORIZONTAL);
+			
+		} else if (orientation == Orientation.LEFT_BOTTOM) {
+			filters.add(Rotation.LEFT_90_DEGREES);
+		}
+		
+		return filters;
+	}
+}
