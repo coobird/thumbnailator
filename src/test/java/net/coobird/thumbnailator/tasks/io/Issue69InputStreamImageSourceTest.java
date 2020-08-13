fb7e2987eca0c960d1abc4a51432646245042f54
Chris Kroells <coobird@coobird.net>
2020-08-13 23:25:43 +0900

    Fix issue with tests writing JPEGs are failing in OpenJDK.
    Tests were failing with "javax.imageio.IIOException: Invalid argument to native writeImage" message.
    This was due to OpenJDK's JPEG writer failing when given a `BufferedImage.TYPE_INT_RGB` image.

--- src/test/java/net/coobird/thumbnailator/tasks/io/Issue69InputStreamImageSourceTest.java
+++ src/test/java/net/coobird/thumbnailator/tasks/io/Issue69InputStreamImageSourceTest.java
@@ -63,7 +63,7 @@ public class Issue69InputStreamImageSourceTest {
 	public static int SIZE = 8000;
 	
 	static {
-		BufferedImage img = new BufferedImage(SIZE, SIZE, BufferedImage.TYPE_INT_ARGB);
+		BufferedImage img = new BufferedImage(SIZE, SIZE, BufferedImage.TYPE_INT_RGB);
 		Graphics2D g = img.createGraphics();
 		g.setPaint(new GradientPaint(0, 0, Color.blue, SIZE, SIZE, Color.red));
 		g.dispose();
