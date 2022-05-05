ea2160f7c39d3659c2f66b3da825e5bdd9d5123d
Chris Kroells <coobird@coobird.net>
2022-05-06 03:21:11 +0900

    Update copyright date.

--- src/main/java/net/coobird/thumbnailator/tasks/io/OutputStreamImageSink.java
+++ src/main/java/net/coobird/thumbnailator/tasks/io/OutputStreamImageSink.java
@@ -1,7 +1,7 @@
 /*
  * Thumbnailator - a thumbnail generation library
  *
- * Copyright (c) 2008-2020 Chris Kroells
+ * Copyright (c) 2008-2022 Chris Kroells
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
